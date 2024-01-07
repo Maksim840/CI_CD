4b98551a65be1790910e8419304ffd0c420d8ecb
maksimmiksha <@Maksim840>
2024-01-07 20:33:30 +0300

    adfasd

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ import mongoose from 'mongoose'
 import userRouter from "./routes/usersRoutes.js"
 
 
-mongoose
-    .connect('mongodb+srv://Maksim:4CBP3zDuJ7UJCbHI@maksim.ppszojk.mongodb.net/users',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    )
-    .then(() => console.log("Database is ok"))
-    .catch((err) => console.log("Database is not ok", err))
+// mongoose
+//     .connect('mongodb+srv://Maksim:4CBP3zDuJ7UJCbHI@maksim.ppszojk.mongodb.net/users',
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//       }
+//     )
+//     .then(() => console.log("Database is ok"))
+//     .catch((err) => console.log("Database is not ok", err))
 
 
 
@@ -22,9 +22,16 @@ app.use(express.json())
 app.use('/api', userRouter)
 
 // Starting the server on port 8000
-app.listen(8000, (err) => {
-  if(err) {
-      return console.log(err);
-  }
-  console.log("Server is running on port 8000");
+// app.listen(8000, (err) => {
+//   if(err) {
+//       return console.log(err);
+//   }
+//   console.log("Server is running on port 8000");
+// });
+
+
+
+export const server = app.listen(8000, () => {
+  console.log('Starting the server on port 8000');
+  mongoose.connect('mongodb+srv://Maksim:4CBP3zDuJ7UJCbHI@maksim.ppszojk.mongodb.net/users')
 });
